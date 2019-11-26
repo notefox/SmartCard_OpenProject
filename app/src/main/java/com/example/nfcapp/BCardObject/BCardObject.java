6ddfc8ec64992e2e4bd85b0fc8d7a857011142ea
notefox <kitsu-geissler@t-online.de>
2019-11-26 18:15:06 +0100

    currently working on BCardObject
    Signed-off-by: notefox <kitsu-geissler@t-online.de>

--- app/src/main/java/com/example/nfcapp/BCardObject/BCardObject.java
+++ app/src/main/java/com/example/nfcapp/BCardObject/BCardObject.java
@@ -1,41 +1,84 @@
 package com.example.nfcapp.BCardObject;
 
 import android.content.ContentValues;
-import android.provider.ContactsContract;
+import android.location.Location;
 import android.provider.ContactsContract.CommonDataKinds.*;
-import android.provider.MediaStore;
+
+import com.google.gson.Gson;
+
+import java.util.Random;
 
 public class BCardObject {
 
-    private int id;
+    private Random rand;
 
-    private ContentValues values;
+    private ContentValues user_values;
+    private ContentValues job_values;
 
-    private StructuredName user;
-    private Organization job;
-    private StructuredPostal address;
-    private Phone landlineNumber;
-    private Phone mobileNumber;
+    private ContentValues mobile_values;
+    private ContentValues landlines_values;
 
-
-    public BCardObject(int id, StructuredName user, Organization job, StructuredPostal address,
-                       Phone landlineNumber, Phone mobileNumber) {
-
-        this.user = user;
-        this.job = job;
-        this.address = address;
-        this.landlineNumber = landlineNumber;
-        this.mobileNumber = mobileNumber;
-
+    public BCardObject(ContentValues user_values, ContentValues job_values, ContentValues mobile_values, ContentValues landline_values) {
+        this.user_values = user_values;
+        this.job_values = job_values;
+        this.mobile_values = mobile_values;
+        this.landlines_values = landline_values;
     }
 
-    void addID() {
-        values.put(StructuredName.CONTACT_ID, "ID");
+    public BCardObject() {
 
-        values.put(StructuredName.PREFIX, "Prefix(Dr., Ing., Prof., ...)");
-        values.put(StructuredName.GIVEN_NAME, "First Name");
-        values.put(StructuredName.MIDDLE_NAME, "Middle Name");
-        values.put(StructuredName.FAMILY_NAME, "Last Name");
-        values.put(StructuredName.SUFFIX, "Suffix");
+        user_values = new ContentValues(6);
+        job_values = new ContentValues(4);
+        mobile_values = new ContentValues(1);
+        landlines_values = new ContentValues(1);
+
+            addUserValues(rand.nextInt(), String.valueOf(rand.nextInt()), String.valueOf(rand.nextInt()), String.valueOf(rand.nextInt()), String.valueOf(rand.nextInt()), String.valueOf(rand.nextInt()));
+            addJobValues(String.valueOf(rand.nextInt()),String.valueOf(rand.nextInt()) ,null, String.valueOf(rand.nextInt()));
+            addLandlinesValues(String.valueOf(rand.nextInt()));
+            addMobileValues(String.valueOf(rand.nextInt()));
     }
+
+    void addUserValues(int id, String prefix, String firstName, String middleName, String lastName, String suffix) {
+
+        user_values.put(StructuredName.CONTACT_ID, id);
+
+        user_values.put(StructuredName.PREFIX, prefix);
+        user_values.put(StructuredName.GIVEN_NAME, firstName);
+        user_values.put(StructuredName.MIDDLE_NAME, middleName);
+        user_values.put(StructuredName.FAMILY_NAME, lastName);
+        user_values.put(StructuredName.SUFFIX, suffix);
+    }
+
+    void addJobValues(String name, String title, Location location, String jobDescription) {
+
+        job_values.put(Organization.COMPANY, name);
+        job_values.put(Organization.TITLE, title);
+        job_values.put(Organization.OFFICE_LOCATION, location.toString());
+        job_values.put(Organization.JOB_DESCRIPTION, jobDescription);
+    }
+
+    void addMobileValues(String number) {
+        mobile_values.put(Phone.NUMBER,number);
+        mobile_values.put(Phone.TYPE,Phone.TYPE_MOBILE);
+    }
+
+    void addLandlinesValues(String number) {
+        mobile_values.put(Phone.NUMBER, number);
+        mobile_values.put(Phone.TYPE, Phone.TYPE_MAIN);
+    }
+
+    @Override
+    public String toString() {
+        return "BCardObject{" +
+                "user_values=" + user_values +
+                ", job_values=" + job_values +
+                ", mobile_values=" + mobile_values +
+                ", landlines_values=" + landlines_values +
+                '}';
+    }
+
+    public String toGsonString() {
+        return new Gson().toJson(this.toString());
+    }
+
 }
--- app/src/main/java/com/example/nfcapp/TestMainClass.java
+++ app/src/main/java/com/example/nfcapp/TestMainClass.java
@@ -1,23 +1,15 @@
 package com.example.nfcapp;
 
+import android.content.ContentValues;
+
+import com.example.nfcapp.BCardObject.BCardObject;
 import com.google.gson.Gson;
+
+import java.util.Random;
 
 public class TestMainClass {
 
-    public String test = "dass";
-    public int testInt = 4234;
-
     public static void main(String[] args) {
-
-        Gson bla = new Gson();
-
-        String a = bla.toJson(new TestMainClass());
-
-        System.out.println(a);
-
-        TestMainClass b = bla.fromJson(a, TestMainClass.class);
-
-        System.out.println(b.testInt + 100000);
-
+        ContentValues values = new ContentValues();
     }
 }
